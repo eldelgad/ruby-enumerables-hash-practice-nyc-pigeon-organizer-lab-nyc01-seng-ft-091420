668d56390052b5b81f1de05c77dbc1c7b40bf95a
Elias Delgado <evdelgado99@gmail.com>
2020-09-03 16:02:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,9 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   
   binding.pry
-  data.each_with_object({}) do |key, value|
+  data = { a: 2, b: 3 }
+    hash.each_with_object({}) do |(k, v), a|
+    a[k] = v + 3
+  end 
 
 end
