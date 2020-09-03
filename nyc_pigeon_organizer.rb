90901ebe58006abe18c30f33680d4f05e40186b4
Elias Delgado <evdelgado99@gmail.com>
2020-09-03 16:19:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,19 +3,25 @@ require 'pry'
 def nyc_pigeon_organizer(data)
 
   data.each_with_object({}) do |(key, value), new_array|
+    
     value.each do |key2, name1|
+      
       names.each do |name2|
-        binding.pry
+        
         if !new_array[name2]
           new_array[name2] = {}
         end
+        
         if !new_array[name2][key]
           !new_array[name2][key] = []
         end
+        
         new_array[name2][key].push(key2)
+        
       end
     end
+    
     new_array
+    
   end 
-  binding.pry
 end
